5781da212ef391562d5e41b8c90d9ba8a2236727
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2025-12-12 18:40:21 -0700

    :rotating_light: Also build project with base URL

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig, PluginOption } from "vite";
 import motionCanvas from "@motion-canvas/vite-plugin";
 import ffmpeg from "@motion-canvas/ffmpeg";
 
+const repoName = process.env.REPO_NAME;
 export default defineConfig(({ command }) => {
   const plugins: PluginOption[] = [motionCanvas()];
   if (command === "serve") {
     plugins.push(ffmpeg());
   }
   return {
+    base: repoName ? `/${repoName}/` : "/",
     plugins,
     build: {
       rollupOptions: {
